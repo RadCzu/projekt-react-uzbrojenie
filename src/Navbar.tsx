040a64bd30416ed7e989231387c85e0d57ff5620
RadCzu <radekczuj@gmail.com>
2024-01-16 20:45:19 +0100

    first test

---
+++ src/Navbar.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import './App.css';
+
+const Navbar = () => {
+  return (  
+    <nav className="navbar">
+      <p>
+        <label>Oręż Średniowiecza</label>
+      </p>
+      <div className="search-bar">   
+        <Link to="/"> 
+          <button className="search-button">
+            <img className="home-icon" src="images/castle.png" />
+          </button>
+        </Link>
+
+        <input id="search-input" type="text" placeholder="Wyszukaj" />
+        <button className="search-button enter-search">
+          <img className="search-icon" src="images/search-icon.png"/>
+        </button>
+
+        <button className="add-review add-weapon">
+          Dodaj Oręż
+        </button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
